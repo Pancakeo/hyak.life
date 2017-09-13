d78ebc42096e69715d195fc510b8b4d4d3d7de0a
thegauntt <jessethegauntt+biz@gmail.com>
2017-09-12 22:01:38 -0700

    Whew.

--- js/whew.js
+++ js/whew.js
@@ -1,5 +1,16 @@
 $(function () {
 
+	var get_page = function (pageKey) {
+		history.pushState({}, "Main", "?page=" + pageKey);
+
+		var $loading = $("#loader").clone().show();
+		$("#page_content").html('').append($loading);
+
+		$.get('/fragments/' + pageKey + '.html?cacheBreak=' + Date.now(), function (res) {
+			$("#page_content").html(res);
+		});
+	};
+
 	/**
 	 * Load a page fragment and render in container.
 	 */
@@ -8,11 +19,7 @@ $(function () {
 		$("#top_links li").removeClass('active');
 		$(this).parent('li').addClass('active');
 
-		history.pushState({}, "Main", "?page=" + pageKey);
-
-		$.get('/fragments/' + pageKey + '.html?cacheBreak=' + Date.now(), function (res) {
-			$("#page_content").html(res);
-		})
+		get_page(pageKey);
 	});
 
 	// If user is refresing, try to return to the previous page:
@@ -20,6 +27,7 @@ $(function () {
 	if (previousPageResults.length == 2) {
 		var prev_page = previousPageResults[1];
 		$("#top_links span[page-key='" + prev_page + "']").click();
+		
 	} else {
 		$("#top_links span[page-key='main']").click();
 	}
